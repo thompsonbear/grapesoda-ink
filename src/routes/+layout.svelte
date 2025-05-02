aa605fd1b3bee09c13ca4290de406b1a8689ba90
Thompson Bear <thompsonbear21@gmail.com>
2025-05-01 23:19:54 -0400

    init/social links

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import '../app.css';
+
+	let { children } = $props();
+</script>
+
+<div class="p-4 grid place-items-center h-screen bg-[url('/textures/noise.png')] bg-purple-50">
+	{@render children()}
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,23 @@
+<div class="flex flex-col justify-center items-center gap-12 text-purple-800">
+    <h1 class="text-3xl font-title text-purple-950">grapesoda.ink</h1>
+    <div class="w-full aspect-square max-w-96 rounded-4xl grid grid-cols-2 grid-rows-2">
+        <div class="flex gap-2 flex-col justify-center items-center border-l-2 border-t-2 border rounded-tl-4xl font-medium tracking-wide">
+            <img src="/images/profile.png" alt="A self portrait of Grape Soda - tattoo artist" class="rounded-full w-3/4 ring-purple-900 ring-2 ring-offset-2">
+        </div>
+        <a href="https://venue.ink/grapesoda.ink" target="_blank" class="flex gap-2 flex-col justify-center items-center border-t-2 border-r-2 border rounded-tr-4xl hover:bg-purple-800/10 bg-[url('/textures/noise.png')] transition-colors font-medium tracking-wide">
+            <svg class="w-[35%] text-purple-900" viewBox="-6 -4 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M6.17479 17.4213C9.6571 17.4213 12.3496 14.6899 12.3496 11.2465C12.3496 7.36344 9.48201 6.13393 6.52496 0.215942C6.381 -0.0719808 5.96857 -0.0719808 5.8285 0.215942C2.86756 6.13393 0 7.36344 0 11.2465C0 14.6899 2.69247 17.4213 6.17479 17.4213Z" fill="currentColor"/>
+            </svg>
+            <span class="text-center">Book a Tattoo</span>
+        </a>
+        <a href="https://www.instagram.com/grapesoda.ink/" target="_blank" class="flex gap-2 flex-col justify-center items-center  border-b-2 border-l-2 border  rounded-bl-4xl hover:bg-purple-800/10 bg-[url('/textures/noise.png')] transition-colors font-medium tracking-wide">
+            <svg xmlns="http://www.w3.org/2000/svg" class="w-[35%] text-purple-900" viewBox="0 0 24 24"><g fill="none"><path d="m12.593 23.258l-.011.002l-.071.035l-.02.004l-.014-.004l-.071-.035q-.016-.005-.024.005l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427q-.004-.016-.017-.018m.265-.113l-.013.002l-.185.093l-.01.01l-.003.011l.018.43l.005.012l.008.007l.201.093q.019.005.029-.008l.004-.014l-.034-.614q-.005-.018-.02-.022m-.715.002a.02.02 0 0 0-.027.006l-.006.014l-.034.614q.001.018.017.024l.015-.002l.201-.093l.01-.008l.004-.011l.017-.43l-.003-.012l-.01-.01z"/><path fill="currentColor" d="M16 3a5 5 0 0 1 5 5v8a5 5 0 0 1-5 5H8a5 5 0 0 1-5-5V8a5 5 0 0 1 5-5zm-4 5a4 4 0 1 0 0 8a4 4 0 0 0 0-8m0 2a2 2 0 1 1 0 4a2 2 0 0 1 0-4m4.5-3.5a1 1 0 1 0 0 2a1 1 0 0 0 0-2"/></g></svg>
+            <span class="text-center">Instagram</span>
+        </a>
+        <a href="https://bsky.app/profile/grapesoda.ink" target="_blank" class="flex gap-2 flex-col justify-center items-center  border-b-2 border-r-2 border  rounded-br-4xl hover:bg-purple-800/10 bg-[url('/textures/noise.png')] transition-colors font-medium tracking-wide">
+            <svg xmlns="http://www.w3.org/2000/svg" class="w-[35%] text-purple-900" viewBox="0 0 24 24"><g fill="none"><path d="m12.594 23.258l-.012.002l-.071.035l-.02.004l-.014-.004l-.071-.036q-.016-.004-.024.006l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427q-.004-.016-.016-.018m.264-.113l-.014.002l-.184.093l-.01.01l-.003.011l.018.43l.005.012l.008.008l.201.092q.019.005.029-.008l.004-.014l-.034-.614q-.005-.019-.02-.022m-.715.002a.02.02 0 0 0-.027.006l-.006.014l-.034.614q.001.018.017.024l.015-.002l.201-.093l.01-.008l.003-.011l.018-.43l-.003-.012l-.01-.01z"/><path fill="currentColor" d="M5.892 2.93c-1.78-1.06-3.813.446-3.63 2.359l.398 4.185c.176 1.855 1.84 4.026 4.195 3.823C4.5 14 3.132 15.865 4.88 18.145c.286.373.597.745.914 1.062s.69.628 1.062.914c1.88 1.441 4.375-.65 5.055-2.69l.09-.268l.09.269c.68 2.04 3.174 4.13 5.055 2.689c.372-.286.745-.597 1.062-.914s.628-.69.914-1.062c1.747-2.28.379-4.145-1.976-4.848c2.355.203 4.018-1.968 4.195-3.823l.399-4.185c.182-1.913-1.852-3.418-3.631-2.36C15.2 4.66 13.485 7.393 12 9.902C10.515 7.392 8.8 4.66 5.892 2.93"/></g></svg>
+            <span class="text-center">Bluesky</span>
+        </a>
+    </div>
+</div>
+
